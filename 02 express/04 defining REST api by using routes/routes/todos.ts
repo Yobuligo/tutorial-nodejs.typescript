506dbb3512b13a134889bd61c6bc4b6780b39ff8
Peter <hoffmannpeter82@gmail.com>
2023-06-22 18:22:56 +0200

    Add type casting to first REST api lesson

--- 02 express/04 defining REST api by using routes/routes/todos.ts
+++ 02 express/04 defining REST api by using routes/routes/todos.ts
@@ -5,21 +5,28 @@ const router = Router();
 
 let todos: ITodo[] = [];
 
+type RequestBody = { text: string };
+type RequestParams = { todoId: string };
+
 router.get("/", (req, res, next) => {
   res.status(200).json({ todos: todos });
 });
 
 router.post("/todo", (req, res, next) => {
-  const newTodo: ITodo = { id: crypto.randomUUID(), text: req.body.text };
+  const body = req.body as RequestBody;
+  const newTodo: ITodo = { id: crypto.randomUUID(), text: body.text };
   todos.push(newTodo);
   res.status(201).json({ message: "Added todo", todo: newTodo, todos: todos });
 });
 
 router.put("/todo/:todoId", (req, res, next) => {
-  const todoId = req.params.todoId;
-  const todoIndex = todos.findIndex((todoItem) => todoItem.id === todoId);
+  const params: RequestParams = req.params;
+  const body: RequestBody = req.body;
+  const todoIndex = todos.findIndex(
+    (todoItem) => todoItem.id === params.todoId
+  );
   if (todoIndex >= 0) {
-    todos[todoIndex] = { id: todoId, text: req.body.text };
+    todos[todoIndex] = { id: params.todoId, text: body.text };
     return res.status(200).json({ message: "Updated todo", todos: todos });
   }
 
@@ -27,7 +34,8 @@ router.put("/todo/:todoId", (req, res, next) => {
 });
 
 router.delete("/todo/:todoId", (req, res, next) => {
-  todos = todos.filter((todoItem) => todoItem.id !== req.params.todoId);
+  const params: RequestParams = req.params;
+  todos = todos.filter((todoItem) => todoItem.id !== params.todoId);
   res.status(200).json({ message: "Delete todo", todos: todos });
 });
 
