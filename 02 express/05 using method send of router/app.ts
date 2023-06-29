9a1fbadbc26e485a963c5354d30a6f5cfd789061
Peter <hoffmannpeter82@gmail.com>
2023-06-29 21:10:13 +0200

    Add lesson for using method send of router

---
+++ 02 express/05 using method send of router/app.ts
@@ -0,0 +1,28 @@
+/**
+ * The method send of a router can be used to send e.g. text or json.
+ * The content type is automatically set to "text/html" e.g. or "application/json". Depending on your data. That is done by express
+ */
+
+import bodyParser from "body-parser";
+import express, { Router } from "express";
+
+const router = Router();
+
+/**
+ * This endpoint sends a text and the content type is automatically set text/html
+ */
+router.get("/text", (req, res) => {
+  res.send("<h1>Hello World</h1>");
+});
+
+/**
+ * This endpoint sends a text and the content type is automatically set application/json
+ */
+router.get("/json", (req, res) => {
+  res.send({ message: "Hello World" });
+});
+
+const server = express();
+server.use(bodyParser.json());
+server.use(router);
+server.listen(5000);
