34a46cde6233256734b9efd2c9e2ff31eac97c75
Peter <hoffmannpeter82@gmail.com>
2023-06-29 21:12:42 +0200

    Extend documentation of using method send of router

--- dist/05 using method send of router/app.js
+++ dist/05 using method send of router/app.js
@@ -2,6 +2,8 @@
 /**
  * The method send of a router can be used to send e.g. text or json.
  * The content type is automatically set to "text/html" e.g. or "application/json". Depending on your data. That is done by express
+ *
+ * This information can be get within the browser, by opening the developer tools with F12 -> choosing network -> selecting the called endpoint -> and all details are displayed
  */
 var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
     if (k2 === undefined) k2 = k;
