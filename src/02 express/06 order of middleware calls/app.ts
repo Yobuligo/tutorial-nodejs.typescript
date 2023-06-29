adfdb7ab7b8e65ffeab608768c787eff047fd7ff
Peter <hoffmannpeter82@gmail.com>
2023-06-29 21:30:46 +0200

    Extend lesson order of middleware by path that always runs

--- src/02 express/06 order of middleware calls/app.ts
+++ src/02 express/06 order of middleware calls/app.ts
@@ -8,6 +8,15 @@ import express from "express";
 const server = express();
 
 /**
+ * The first middleware will always run.
+ * By calling next, it ensures that at least the next middleware is called.
+ */
+server.use("/", (req, res, next) => {
+  console.log("This always runs!");
+  next();
+});
+
+/**
  * This middleware is called before the next one /second.
  * Which means whenever a request is already handled, e.g. the next route won't be called.
  * To force the call of the next middleware, the function next has to be called.
@@ -21,5 +30,7 @@ server.use("/first", (req, res, next) => {
  * This middleware would only be called if next was called in the previous caller or no specified route was found before
  */
 server.use("/second", () => {});
+
+server.use(() => {});
 
 server.listen(5000);
